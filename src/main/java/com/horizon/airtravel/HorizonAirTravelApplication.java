18e2a3cc522b5c38f098f8c7b1b65eeab6681b01
Silas Caimi <silascaimi@live.com>
2023-08-29 21:29:53 -0300

    Initial commit

---
+++ src/main/java/com/horizon/airtravel/HorizonAirTravelApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.airtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonAirTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonAirTravelApplication.class, args);
+	}
+
+}
